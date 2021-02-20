3f6f426e8e933e5d0f64565e6ffa01a5b9fc1789
Aaron Bassett <me@aaronbassett.com>
2021-02-20 13:49:05 -0500

    Create add task form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,16 +5,17 @@ import { CheckCircleOutlined, MinusCircleOutlined } from "@ant-design/icons"
 import { useEffect, useState } from "react"
 
 function App() {
+  const [newTask, setNewTask] = useState("")
   const [tasks, setTasks] = useState([])
   const [timeline, setTimeline] = useState([])
 
   const formLayout = {
-    labelCol: { span: 8 },
-    wrapperCol: { span: 16 },
+    labelCol: { span: 10 },
+    wrapperCol: { span: 14 },
   }
 
   const formLayoutTail = {
-    wrapperCol: { offset: 8, span: 16 },
+    wrapperCol: { offset: 11, span: 13 },
   }
 
   useEffect(() => {
@@ -55,8 +56,19 @@ function App() {
     setTimeline(timelineItems)
   }, [tasks])
 
-  const onFinish = () => {
-    return
+  const saveTask = async () => {
+    if (newTask) {
+      await fetch("/task/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: newTask,
+          completed: false,
+        }),
+      })
+    }
   }
 
   return (
@@ -65,17 +77,21 @@ function App() {
         <Divider>FARM Stack TODO App</Divider>
         <Row gutter={16}>
           <Col span={6}>
-            <Form {...formLayout} name="todoForm" onFinish={onFinish}>
+            <Form {...formLayout} name="todoForm">
               <Form.Item
                 label="TODO Item"
                 name="TODOItem"
                 rules={[{ required: true, message: "Please input a TODO" }]}
               >
-                <Input />
+                <Input
+                  allowClear={true}
+                  value={newTask}
+                  onChange={(e) => setNewTask(e.target.value)}
+                />
               </Form.Item>
 
               <Form.Item {...formLayoutTail}>
-                <Button type="primary" htmlType="submit">
+                <Button type="primary" htmlType="submit" onClick={saveTask}>
                   Add New Todo
                 </Button>
               </Form.Item>
