62b7c1add6476f3081d3b18b3eeaa0444f110c05
Aaron Bassett <me@aaronbassett.com>
2021-02-20 13:23:32 -0500

    Add static React layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+
+import { Button, Col, Divider, Form, Input, Row, Timeline } from "antd"
+import { CheckCircleOutlined, MinusCircleOutlined } from "@ant-design/icons"
 
 function App() {
+  const formLayout = {
+    labelCol: { span: 8 },
+    wrapperCol: { span: 16 },
+  }
+
+  const formLayoutTail = {
+    wrapperCol: { offset: 8, span: 16 },
+  }
+
+  const onFinish = () => {
+    return
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className={"content"}>
+        <Divider>FARM Stack TODO App</Divider>
+        <Row gutter={16}>
+          <Col span={6}>
+            <Form {...formLayout} name="todoForm" onFinish={onFinish}>
+              <Form.Item
+                label="TODO Item"
+                name="TODOItem"
+                rules={[{ required: true, message: "Please input a TODO" }]}
+              >
+                <Input />
+              </Form.Item>
+
+              <Form.Item {...formLayoutTail}>
+                <Button type="primary" htmlType="submit">
+                  Add New Todo
+                </Button>
+              </Form.Item>
+            </Form>
+          </Col>
+          <Col span={18}>
+            <Timeline mode="alternate">
+              <Timeline.Item
+                dot={<MinusCircleOutlined />}
+                color="blue"
+                style={{ textDecoration: "initial" }}
+              >
+                Task Name <small>(123)</small>
+              </Timeline.Item>
+            </Timeline>
+          </Col>
+        </Row>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
