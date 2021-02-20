013acadbe104515b9f07a5b502b8df9b28003a53
Aaron Bassett <me@aaronbassett.com>
2021-02-20 12:35:53 -0500

    Connect to MongoDB

---
+++ backend/main.py
@@ -0,0 +1,27 @@
+import uvicorn
+from fastapi import FastAPI
+from motor.motor_asyncio import AsyncIOMotorClient
+
+from config import settings
+
+app = FastAPI()
+
+
+@app.on_event("startup")
+async def startup_db_client():
+    app.mongodb_client = AsyncIOMotorClient(settings.DB_URL)
+    app.mongodb = app.mongodb_client[settings.DB_NAME]
+
+
+@app.on_event("shutdown")
+async def shutdown_db_client():
+    app.mongodb_client.close()
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        host=settings.HOST,
+        reload=settings.DEBUG_MODE,
+        port=settings.PORT,
+    )
